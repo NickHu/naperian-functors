b50078d1f9295781fd09b6d038320f8457038b14
Nick Hu <me@nickhu.co.uk>
2017-12-30 23:37:00 +0000

    add Vector cons, snoc, ++, concat, generate

--- src/Data/Naperian/Vector.hs
+++ src/Data/Naperian/Vector.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveTraversable   #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE MagicHash           #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 
 {-|
 Module      : Data.Naperian.Vector
@@ -23,15 +23,15 @@ Implemented with @Data.Vector@.
 
 module Data.Naperian.Vector where
 
-import Data.Kind (Type)
-import Data.Maybe (fromMaybe)
-import GHC.Exts (IsList(..))
-import GHC.Prim (proxy#, Proxy#)
-import GHC.TypeLits
 import qualified Data.Foldable as F
-import qualified Data.Vector as V
+import           Data.Kind     (Type)
+import           Data.Maybe    (fromMaybe)
+import qualified Data.Vector   as V
+import           GHC.Exts      (IsList (..))
+import           GHC.Prim      (Proxy#, proxy#)
+import           GHC.TypeLits
 
-import Data.Naperian
+import           Data.Naperian
 
 newtype Vector (n :: Nat) a = Vector (V.Vector a)
   deriving (Eq, Show, Ord, Functor, Foldable, Traversable)
@@ -48,6 +48,24 @@ data Finite :: Nat -> Type where
 finite :: forall n. KnownNat n => Int -> Maybe (Finite n)
 finite x = if x > y then Nothing else Just (Fin x)
   where y = fromIntegral (natVal' (proxy# :: Proxy# n))
+
+cons :: a -> Vector n a -> Vector (n + 1) a
+cons x (Vector v) = Vector $ V.cons x v
+
+snoc :: Vector n a -> a -> Vector (n + 1) a
+snoc (Vector v) x = Vector $ V.snoc v x
+
+(++) :: Vector m a -> Vector m a -> Vector (m + n) a
+(++) (Vector u) (Vector v) = Vector $ (V.++) u v
+
+concat :: Vector m (Vector n a) -> Vector (m * n) a
+concat (Vector u) = Vector $ V.concat (map unvector $ V.toList u)
+  where unvector :: Vector n a -> V.Vector a
+        unvector (Vector v) = v
+
+generate :: forall n a. KnownNat n => (Int -> a) -> Vector n a
+generate f = Vector $ V.generate s f
+  where s = fromIntegral $ natVal' (proxy# :: Proxy# n) :: Int
 
 tail :: Vector n a -> Vector (n - 1) a
 tail (Vector v) = Vector $ V.tail v
